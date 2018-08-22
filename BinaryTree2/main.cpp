d4288805ce7326652ccdd76a684c5015ac6cbde8
prometeyqwe <kirilllov.evgeny@mail.ru>
2018-08-22 17:04:23 +0300

    Initial Commit

---
+++ BinaryTree2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BinaryTree2
+//
+//  Created by Евгений Киримов on 22.08.2018.
+//  Copyright © 2018 Евгений Киримов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
